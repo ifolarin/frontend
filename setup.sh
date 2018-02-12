bd9ae3cb6e941bb2ac5b59751600853886d42f1e
Ibrahim Olukorede Folarin <ifolarin@webbasedsol.com>
2018-02-12 18:40:23 +0100

    fixed path to setup-firewall.sh and removed container startup

--- setup.sh
+++ setup.sh
@@ -6,9 +6,5 @@ docker network create web-proxy
 echo -e "...done."
 
 # Call setup-firewall.sh
-setup-firewall.sh
-echo -e "...done."
-
-echo -e "Starting frontend..."
-docker-compose up -d
+$(pwd)/setup-firewall.sh
 echo -e "...done."
